67791fadd079c823e2d62feb89ea3d339bad516a
Luke Berry <contact@luke-berry.co.uk>
2017-11-23 05:05:10 +0000

    squash

--- src/server.js
+++ src/server.js
@@ -10,4 +10,4 @@ app.get('/', (req, res) => {
 
 
 // Listen on port 3000
-app.listen(process.env.port || 3000, () => console.log('Server started on port 3000!'))
+app.listen(3000, () => console.log('Server started on port 3000!'))
